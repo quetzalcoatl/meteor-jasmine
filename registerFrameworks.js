0be15356acb394e374904172cca68b449b9cd72e
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-11-24 14:06:34 +0100

    Allow to disable test modes

--- registerFrameworks.js
+++ registerFrameworks.js
@@ -1,23 +1,50 @@
 
-frameworks = {
-  //serverIntegration: new ServerIntegrationTestFramework(),
-  clientIntegration: new ClientIntegrationTestFramework(),
-  clientUnit: new ClientUnitTestFramework(),
-  serverUnit: new ServerUnitTestFramework()
+frameworks = {}
+
+if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
+  frameworks.clientIntegration = new ClientIntegrationTestFramework()
+}
+
+if (process.env.JASMINE_CLIENT_UNIT !== '0') {
+  frameworks.clientUnit = new ClientUnitTestFramework()
+}
+
+/*
+if (process.env.JASMINE_SERVER_INTEGRATION !== '0') {
+  frameworks.serverIntegration = new ServerIntegrationTestFramework()
+}
+*/
+
+if (process.env.JASMINE_SERVER_UNIT !== '0') {
+  frameworks.serverUnit = new ServerUnitTestFramework()
 }
 
 if (process.env.VELOCITY !== '0' && !process.env.IS_MIRROR) {
-  frameworks.clientIntegration.registerWithVelocity()
-  frameworks.clientIntegration.startFileCopier()
+  if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
+    frameworks.clientIntegration.registerWithVelocity()
+    frameworks.clientIntegration.startFileCopier()
+  }
 
-  frameworks.clientUnit.registerWithVelocity()
+  if (process.env.JASMINE_CLIENT_UNIT !== '0') {
+    frameworks.clientUnit.registerWithVelocity()
+  }
 
-  frameworks.serverUnit.registerWithVelocity()
+  if (process.env.JASMINE_SERVER_UNIT !== '0') {
+    frameworks.serverUnit.registerWithVelocity()
+  }
 
   Meteor.startup(function () {
-    frameworks.clientIntegration.startMirror()
-    //frameworks.serverIntegration.runTests()
-    frameworks.clientUnit.start()
-    frameworks.serverUnit.start()
+    if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
+      frameworks.clientIntegration.startMirror()
+    }
+    //if (process.env.JASMINE_SERVER_INTEGRATION !== '0') {
+      //frameworks.serverIntegration.runTests()
+    //}
+    if (process.env.JASMINE_CLIENT_UNIT !== '0') {
+      frameworks.clientUnit.start()
+    }
+    if (process.env.JASMINE_SERVER_UNIT !== '0') {
+      frameworks.serverUnit.start()
+    }
   })
 }
