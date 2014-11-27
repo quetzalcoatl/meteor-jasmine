ba76d7f63ecaf17ebd640af49e29eab6568ff5c4
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-11-28 00:18:38 +0100

    Start FileCopier in Meteor.startup to maybe fix a rare copy error

--- registerFrameworks.js
+++ registerFrameworks.js
@@ -22,7 +22,6 @@ if (process.env.JASMINE_SERVER_UNIT !== '0') {
 if (process.env.VELOCITY !== '0' && !process.env.IS_MIRROR) {
   if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
     frameworks.clientIntegration.registerWithVelocity()
-    frameworks.clientIntegration.startFileCopier()
   }
 
   if (process.env.JASMINE_CLIENT_UNIT !== '0') {
@@ -35,6 +34,7 @@ if (process.env.VELOCITY !== '0' && !process.env.IS_MIRROR) {
 
   Meteor.startup(function () {
     if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
+      frameworks.clientIntegration.startFileCopier()
       frameworks.clientIntegration.startMirror()
     }
     //if (process.env.JASMINE_SERVER_INTEGRATION !== '0') {
