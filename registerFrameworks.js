1b6dd681cc610d29320d2fb2e79cd206c0974c8d
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-11-28 16:22:02 +0100

    Fixes mirror starting problem when Client Integration mode is disabled

--- registerFrameworks.js
+++ registerFrameworks.js
@@ -38,6 +38,7 @@ if (process.env.VELOCITY !== '0') {
     Meteor.startup(function () {
       if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
         frameworks.clientIntegration.startFileCopier()
+        frameworks.clientIntegration.startMirror()
 
       }
       if (process.env.JASMINE_CLIENT_UNIT !== '0') {
@@ -45,12 +46,10 @@ if (process.env.VELOCITY !== '0') {
       }
       if (process.env.JASMINE_SERVER_INTEGRATION !== '0') {
         frameworks.serverIntegration.startFileCopier()
-      }
-      if (process.env.JASMINE_CLIENT_INTEGRATION !== '0' ||
-          process.env.JASMINE_SERVER_INTEGRATION !== '0'
-      ) {
-        // Use one mirror for now
-        frameworks.clientIntegration.startMirror()
+        if (process.env.JASMINE_CLIENT_INTEGRATION === '0') {
+          // Use one mirror for now
+          frameworks.serverIntegration.startMirror()
+        }
       }
       if (process.env.JASMINE_SERVER_UNIT !== '0') {
         frameworks.serverUnit.start()
