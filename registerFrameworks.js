a7884655b4b8c806342e1f691c752afa049cf904
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-12-11 17:48:24 +0100

    Do nothing when VELOCITY=0 is set

--- registerFrameworks.js
+++ registerFrameworks.js
@@ -1,23 +1,23 @@
 
 frameworks = {}
 
-if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
-  frameworks.clientIntegration = new ClientIntegrationTestFramework()
-}
-
-if (process.env.JASMINE_CLIENT_UNIT !== '0') {
-  frameworks.clientUnit = new ClientUnitTestFramework()
-}
-
-if (process.env.JASMINE_SERVER_INTEGRATION !== '0') {
-  frameworks.serverIntegration = new ServerIntegrationTestFramework()
-}
-
-if (process.env.JASMINE_SERVER_UNIT !== '0') {
-  frameworks.serverUnit = new ServerUnitTestFramework()
-}
-
 if (process.env.VELOCITY !== '0') {
+  if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
+    frameworks.clientIntegration = new ClientIntegrationTestFramework()
+  }
+
+  if (process.env.JASMINE_CLIENT_UNIT !== '0') {
+    frameworks.clientUnit = new ClientUnitTestFramework()
+  }
+
+  if (process.env.JASMINE_SERVER_INTEGRATION !== '0') {
+    frameworks.serverIntegration = new ServerIntegrationTestFramework()
+  }
+
+  if (process.env.JASMINE_SERVER_UNIT !== '0') {
+    frameworks.serverUnit = new ServerUnitTestFramework()
+  }
+
   if (!process.env.IS_MIRROR) {
     if (process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
       frameworks.clientIntegration.registerWithVelocity()
