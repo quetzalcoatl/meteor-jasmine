1654c59a1ca6b63a10b6a1fad3116eb7036fdaa9
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-01-31 22:40:13 +0100

    Use latest version of sanjo:karma
    * Use Jasmine 2.1 in client unit mode
    * Fixes some issues in Karma for client unit mode

--- package.js
+++ package.js
@@ -40,7 +40,7 @@ Package.onUse(function (api) {
   api.use([
     'velocity:meteor-stubs@1.0.0_2',
     'alanning:package-stubber@0.0.9',
-    'sanjo:karma@1.2.0'
+    'sanjo:karma@1.3.0'
   ], 'server')
 
   api.use([
