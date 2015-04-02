db25ee4899c821b323515d4a5d55a280f1a2f76d
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-02 12:03:47 +0200

    Make client integration mode work with new mirror

--- registerFrameworks.js
+++ registerFrameworks.js
@@ -10,56 +10,57 @@ function shouldRun(frameworkName) {
 }
 
 if (process.env.VELOCITY !== '0') {
-  frameworks.serverIntegration = new ServerIntegrationTestFramework()
 
-  if (isMainApp()) {
-    frameworks.serverIntegration.registerWithVelocity()
-    frameworks.serverIntegration.startMirror();
-  }
+  // Server Integration
+  if (!process.env.JASMINE_SERVER_INTEGRATION !== '0') {
+    frameworks.serverIntegration = new ServerIntegrationTestFramework()
 
-  if (shouldRun('jasmine-server-integration')) {
-    if (process.env.IS_MIRROR) {
-      Meteor.startup(
-        function () {
-          frameworks.serverIntegration.start()
-        }
-      )
+    if (isMainApp()) {
+      frameworks.serverIntegration.registerWithVelocity()
+      frameworks.serverIntegration.startMirror()
+    }
+
+    if (shouldRun('jasmine-server-integration')) {
+      Meteor.startup(function () {
+        frameworks.serverIntegration.start()
+      })
     }
   }
 
-  if (process.env.JASMINE_CLIENT_INTEGRATION === '1') {
+
+  // Client Integration
+  if (!process.env.JASMINE_CLIENT_INTEGRATION !== '0') {
     frameworks.clientIntegration = new ClientIntegrationTestFramework()
+
+    if (isMainApp()) {
+      frameworks.clientIntegration.registerWithVelocity()
+      frameworks.clientIntegration.startMirror()
+    }
   }
 
-  if (process.env.JASMINE_CLIENT_UNIT === '1') {
+
+  if (process.env.JASMINE_CLIENT_UNIT !== '0') {
     frameworks.clientUnit = new ClientUnitTestFramework()
   }
 
-  if (process.env.JASMINE_SERVER_UNIT === '1') {
+  if (process.env.JASMINE_SERVER_UNIT !== '0') {
     frameworks.serverUnit = new ServerUnitTestFramework()
   }
 
   if (!process.env.IS_MIRROR) {
-    if (process.env.JASMINE_CLIENT_INTEGRATION === '1') {
-      frameworks.clientIntegration.registerWithVelocity()
-    }
-
-    if (process.env.JASMINE_CLIENT_UNIT === '1') {
+    if (process.env.JASMINE_CLIENT_UNIT !== '0') {
       frameworks.clientUnit.registerWithVelocity()
     }
 
-    if (process.env.JASMINE_SERVER_UNIT === '1') {
+    if (process.env.JASMINE_SERVER_UNIT !== '0') {
       frameworks.serverUnit.registerWithVelocity()
     }
 
     Velocity.startup(function () {
-      if (process.env.JASMINE_CLIENT_INTEGRATION === '1') {
-        frameworks.clientIntegration.startMirror()
-      }
-      if (process.env.JASMINE_CLIENT_UNIT === '1') {
+      if (process.env.JASMINE_CLIENT_UNIT !== '0') {
         frameworks.clientUnit.start()
       }
-      if (process.env.JASMINE_SERVER_UNIT === '1') {
+      if (process.env.JASMINE_SERVER_UNIT !== '0') {
         frameworks.serverUnit.start()
       }
     })
